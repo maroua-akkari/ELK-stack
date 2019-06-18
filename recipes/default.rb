9ebaea817d5bb75a8f0960c161c09f8b18f3168d
Tech-A29-clean <tech-a29-clean@Tech-A29s-MacBook-Air.local>
2019-06-18 09:10:16 +0100

    added files

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,57 @@ apt_update 'update_sources' do
 end
 
 
-# bash '' do
-#   code 'curl -XGET --globoff "http://localhost:9200/logstash/typename/optionalUniqueId" -d '{ "field" : "value" }''
+
+#
+bash '' do
+  code 'sudo apt-get -y install logstash'
+end
+
+template "/etc/logstash/conf.d/02-beats-input.conf" do
+  owner "root"
+  group "root"
+  mode "0777"
+  source "02-beats-input.conf.erb"
+end
+
+template "/etc/logstash/conf.d/10-syslog-filter.conf" do
+  owner "root"
+  group "root"
+  mode "0777"
+  source "10-syslog-filter.conf.erb"
+end
+
+template "/etc/logstash/conf.d/30-elasticsearch-output.conf" do
+  owner "root"
+  group "root"
+  mode "0777"
+  source "30-elasticsearch-output.conf.erb"
+end
+
+bash '' do
+  code 'sudo service logstash restart'
+end
+
+bash '' do
+  code 'sudo systemctl enable logstash'
+end
+
+bash '' do
+  code 'sudo apt-get install filebeat'
+end
+
+# template "/etc/filebeat/filebeat.yml" do
+#   owner "root"
+#   group "root"
+#   mode "0777"
+#   source "filebeat.yml.erb"
 # end
+
+bash '' do
+  code 'sudo service filebeat start'
+end
+
+bash '' do
+  code 'sudo systemctl enable filebeat'
+end
+#
